cc7d353c6097d1efce0dc8122f1c1381ccdec2b8
Emery <emeryntumba39@outlook.com>
2024-10-03 20:44:25 +0100

    add  fillable attribute for conversation model

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'session_id',
+        'message',
+        'role',
+    ];
 }
