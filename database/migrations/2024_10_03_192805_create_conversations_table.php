df451562bd24f876822c056fb8484dcbdb96290b
Emery NTUMBA <emeryntumba39@outlook.com>
2024-10-05 12:40:17 +0100

    Update 2024_10_03_192805_create_conversations_table.php

--- database/migrations/2024_10_03_192805_create_conversations_table.php
+++ database/migrations/2024_10_03_192805_create_conversations_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
+            $table->string('session_id');
             $table->foreign('session_id')->references('id')->on('sessions')->onDelete('cascade');
             $table->text('message');
             $table->enum('role', ['user', 'bot']);
